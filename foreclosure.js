ccf6a8315566fc9c0d9663fa355b6ffd1d2d07ba
jhoun <jayhoun@gmail.com>
2016-12-07 11:18:09 -1000

    inputted the borrower function

--- foreclosure.js
+++ foreclosure.js
@@ -5,5 +5,68 @@ var month = 0;
 var monthsUntilEvicted;
 
 function loan(){
+ var account = {
+  borrowed: 550000,
+  balance: 286000,
+  monthlyPayment: 1700,
+  defaulted: 0,
+  defaultsToForeclose: 5,
+  foreclosed: false
+};
 
+function missPayment(){
+  account.defaulted += 1;
+  if (account.defaulted >= account.defaultsToForeclose){
+    account.forclosed = true;
+  }
+}
+
+return {
+  getBalance: function(){
+    return account.balance;
+  },
+  receivePayment: function(amount){
+    if (amount < account.monthlyPayment){
+      missPayment();
+    }
+    else {
+      account.balance -= amount;
+    }
+  },
+  getMonthlyPayment: function(){
+    return account.monthlyPayment;
+  },
+  isForeclosed: function(){
+    return account.foreclosed;
+  }
+  };
+}
+
+stevesLoan = loan();
+
+function borrower(loan){
+  var account = {
+    monthlyIncome: 1350,
+    funds: 2800,
+    loan: loan
+  }
+
+  return {
+    getFunds: function() {
+      return account.funds;
+    },
+    makePayment: function() {
+      if(account.funds > loan.getMonthlyPayment()) {
+        account.funds = account.funds - loan.getMonthlyPayment();
+        loan.recievePayment(loan.getMonthlyPayment());
+      }
+      else {
+        loan.recievePayment(account.funds);
+        account.funds = 0;
+      }
+    },
+    payDay: function() {
+      account.funds = account.funds + account.monthlyIncome;
+    }
+  }
 };
